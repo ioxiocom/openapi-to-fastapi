3cb2c5a86216442a34a76cc3a4c90950e959a44f
Joakim Nordling <joakim.nordling@ioxio.com>
2024-05-27 15:57:43 +0300

    Update FastAPI, pydantic and datamodel-code-generator

--- openapi_to_fastapi/tests/snapshots/snap_test_router.py
+++ openapi_to_fastapi/tests/snapshots/snap_test_router.py
@@ -13,14 +13,12 @@ snapshots["test_custom_route_definitions Custom route definition"] = {
             "loc": ["query", "vendor"],
             "msg": "Field required",
             "type": "missing",
-            "url": "https://errors.pydantic.dev/2.4/v/missing",
         },
         {
             "input": None,
             "loc": ["header", "auth-header"],
             "msg": "Field required",
             "type": "missing",
-            "url": "https://errors.pydantic.dev/2.4/v/missing",
         },
     ]
 }
@@ -32,7 +30,6 @@ snapshots["test_weather_route_payload_errors Incorrect payload type"] = {
             "loc": ["body", "lat"],
             "msg": "Input should be a valid number, unable to parse string as a number",
             "type": "float_parsing",
-            "url": "https://errors.pydantic.dev/2.4/v/float_parsing",
         },
         {
             "ctx": {"le": 180.0},
@@ -40,7 +37,6 @@ snapshots["test_weather_route_payload_errors Incorrect payload type"] = {
             "loc": ["body", "lon"],
             "msg": "Input should be less than or equal to 180",
             "type": "less_than_equal",
-            "url": "https://errors.pydantic.dev/2.4/v/less_than_equal",
         },
     ]
 }
@@ -52,14 +48,12 @@ snapshots["test_weather_route_payload_errors Missing payload"] = {
             "loc": ["body", "lat"],
             "msg": "Field required",
             "type": "missing",
-            "url": "https://errors.pydantic.dev/2.4/v/missing",
         },
         {
             "input": {},
             "loc": ["body", "lon"],
             "msg": "Field required",
             "type": "missing",
-            "url": "https://errors.pydantic.dev/2.4/v/missing",
         },
     ]
 }
